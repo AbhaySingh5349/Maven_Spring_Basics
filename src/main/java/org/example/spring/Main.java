7b0d1b2304068d9acf86a0cb937192c1b55c6639
AbhaySingh5349 <abhay5349singh@gmail.com>
2024-07-08 13:52:36 +0530

    creating objects using Bean

---
+++ src/main/java/org/example/spring/Main.java
@@ -0,0 +1,15 @@
+package org.example.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        Address address = new Address("250003", "meerut");
+        Student student = new Student("abhay", 1, address);
+
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("factoryBean.xml");
+
+        Student s1 = (Student) applicationContext.getBean("student");
+        System.out.println("s1: " + s1);
+    }
+}
---
+++ src/main/java/org/example/spring/Student.java
@@ -0,0 +1,49 @@
+package org.example.spring;
+
+public class Student {
+    String name;
+    int rollNum;
+    Address address;
+
+    public Student(String name, int rollNum, Address address) {
+        this.name = name;
+        this.rollNum = rollNum;
+        this.address = address;
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNum=" + rollNum +
+                ", address=" + address +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNum() {
+        return rollNum;
+    }
+
+    public void setRollNum(int rollNum) {
+        this.rollNum = rollNum;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
